be0567cea6405409afbdfd9cd42250acccbd0941
Dmitry <kurson.d@gmail.com>
2025-01-15 19:23:02 +0200

    Updated moderator functions: finished show_users method

--- main/moderator/urls.py
+++ main/moderator/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_index, name="index_m"),
+    path('', views.show_index, name="index"),
     path('shop/', views.show_shop, name="show_shop"),
-    path('community/', views.show_community, name="show_community")
-
+    path('community/', views.show_community, name="show_community"),
+    path('users/', views.show_users, name="show_users"),
+    path('user/<int:pk>/delete', views.DeleteView.as_view(), name="user_delete")
 ]
 
 
